5df2f0bd97c12d362f8856e3a774b4dcb1e3e56c
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-12 21:58:15 +0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -102,7 +102,7 @@
     </ul>
     {:else}
     <div><a href="/auth">Login</a></div>
-  
+  {/if}
   <div class="navbar-end">
     <button class="btn btn-ghost btn-circle">
       <div class="indicator">
