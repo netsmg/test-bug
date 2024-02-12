4bb6bf4f759bf7e6d0ff8ebad4b70598349d0ae8
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-13 00:25:00 +0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,63 +1,67 @@
-<script lang="ts">
-  import { onMount } from 'svelte';
-  import { onAuthStateChanged, signOut } from 'firebase/auth';
-  import { themes } from '$lib/themes';
+<script>
+   import "../app.css";
+   import { page } from "$app/stores";
+  import {onAuthStateChanged, signOut} from 'firebase/auth'
   import { fauth } from "../firebase";
-  import { userStore } from "../stores/userStore";
+  import {onMount} from 'svelte';
+  import { themes } from '$lib/themes';
+  import {userStore} from "../stores/userStore"
   import toast, { Toaster } from 'svelte-french-toast';
-  import "../app.css";
+  
   let me;
   let currentTheme = '';
-  
-
-  onMount(() => {
-    document.title = "TESThub | Home";
+  onMount(async () => {
+    document.title = "TESTHub | Home";
     const theme = window.localStorage.getItem('theme');
     if (theme && themes.includes(theme)) {
       document.documentElement.setAttribute('data-theme', theme);
       currentTheme = theme;
     }
-    
-    const saveSettings = () => {
+   const  saveSettings =() => {
       return new Promise((resolve, reject) => {
-        let x = setInterval(() => {
-          if (userStore.loggedIn !== "neutral") {
-            resolve();
-            clearInterval(x);
-          }
-        }, 100);
+        let x = setInterval(()=>{
+          if($userStore.loggedIn===true){
+          resolve();
+          clearInterval(x);
+        }else if($userStore.loggedIn===false){
+          reject();
+          clearInterval(x);
+        }
+        }, 100)
       });
     }
-
     toast.promise(
-      saveSettings(),
-      {
-        loading: 'Checking...',
-        success: userStore.loggedIn === true ? 'Signed In!' : 'You are not signed In!',
-        error: 'Failed to check authentication status!',
-      },
+        saveSettings(),
+        {
+          loading: 'Checking...',
+          success: 'Signed In!',
+          error: 'You are not signed In!',
+        },
     );
-
-    onAuthStateChanged(fauth, async (user) => {
-      if (user) {
-        userStore.set({...user, loggedIn: true});
-        me = user;
-      } else {
-        me = false;
-        userStore.set({loggedIn: false});
-      }
+    onAuthStateChanged(fauth, async(user) => {
+        if(user) {
+            userStore.set({...user, loggedIn: true});
+            me = user;
+        } else{
+           me = false
+            console.log('User Not Logged In!');
+            userStore.set({loggedIn: false});
+        }
+       
     });
+  
   });
 
-  async function logOut() {
-    try {
-      await signOut(fauth);
-    } catch (err) {
-      console.log(err);
-    }
-  }
+  
 
-  function setTheme(event) {
+  const logOut = async() =>{
+      try {
+          await signOut(fauth);
+      } catch (err) {
+        console.log(err);
+      }
+      
+      function setTheme(event) {
     const select = event.target;
     const theme = select.value;
     if (themes.includes(theme)) {
@@ -68,8 +72,8 @@
       currentTheme = theme;
     }
   }
+  
 </script>
-
 <svelte:head>
   
   <link
@@ -84,7 +88,7 @@
   <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 </svelte:head>
-<Toaster />
+<Toaster/>
 
 <div class="navbar bg-base-100">
   <!-- Navbar content -->
@@ -105,19 +109,23 @@
     <a class="btn btn-ghost text-xl">TESThub</a>
   </div>
   <div class="flex-none gap-2">
-    
+    <div class="form-control">
+      <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+    </div>
     <div class="dropdown dropdown-end">
-      {#if userStore.loggedIn === true}
+         {#if me}
+      
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
           <div class="w-10 rounded-full">
             <img src={me.photoURL} alt="Rounded avatar" />
           </div>
           <button on:click={logOut}>Sign out</button>
         </div>
-      {:else if userStore.loggedIn === false}
+        {:else}
+      
         <button class="btn" on:click={() => window.location.href = '/auth'}>Login</button>
-      {:else}
-        <span>Loading...</span>
+     
+        
       {/if}
     </div>
   </div>
