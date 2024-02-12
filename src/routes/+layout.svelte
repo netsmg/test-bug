c18ac2b34f8e89700d286e7415f459cb4b277ec1
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-12 21:51:43 +0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,72 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { themes } from '$lib/themes';
- import '../app.css';
-	let currentTheme = '';
+  import { onMount } from 'svelte';
+  import { themes } from '$lib/themes';
+  import { fauth } from "../firebase";
+  import { onAuthStateChanged, signOut } from 'firebase/auth';
+  import { userStore } from "../stores/userStore";
+  import toast, { Toaster } from 'svelte-french-toast';
 
-	onMount(() => {
-		if (typeof window !== 'undefined') {
-			const theme = window.localStorage.getItem('theme');
-			if (theme && themes.includes(theme)) {
-				document.documentElement.setAttribute('data-theme', theme);
-				currentTheme = theme;
-			}
-		}
-	});
+  let me;
 
-	function setTheme(event: Event) {
-		const select = event.target as HTMLSelectElement;
-		const theme = select.value;
-		if (themes.includes(theme)) {
-			const one_year = 60 * 60 * 24 * 365;
-			window.localStorage.setItem('theme', theme);
-			document.cookie = `theme=${theme}; max-age=${one_year}; path=/; SameSite=Strict;`;
-			document.documentElement.setAttribute('data-theme', theme);
-			currentTheme = theme;
-		}
-	}
+  onMount(async () => {
+    document.title = "TESThub | Home";
+    
+    const saveSettings = () => {
+      return new Promise((resolve, reject) => {
+        let x = setInterval(() => {
+          if (userStore.loggedIn === true) {
+            resolve();
+            clearInterval(x);
+          } else if (userStore.loggedIn === false) {
+            reject();
+            clearInterval(x);
+          }
+        }, 100)
+      });
+    }
+
+    toast.promise(
+      saveSettings(),
+      {
+        loading: 'Checking...',
+        success: 'Signed In!',
+        error: 'You are not signed In!',
+      },
+    );
+
+    onAuthStateChanged(fauth, async (user) => {
+      if (user) {
+        userStore.set({...user, loggedIn: true});
+        me = user;
+      } else {
+        me = false;
+        console.log('User Not Logged In!');
+        userStore.set({loggedIn: false});
+      }
+    });
+  });
+
+  const logOut = async () => {
+    try {
+      await signOut(fauth);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function setTheme(event: Event) {
+    const select = event.target as HTMLSelectElement;
+    const theme = select.value;
+    if (themes.includes(theme)) {
+      const one_year = 60 * 60 * 24 * 365;
+      window.localStorage.setItem('theme', theme);
+      document.cookie = `theme=${theme}; max-age=${one_year}; path=/; SameSite=Strict;`;
+      document.documentElement.setAttribute('data-theme', theme);
+    }
+  }
 </script>
 
-
+<Toaster />
 <div class="navbar bg-base-100">
   <div class="navbar-start">
     <div class="dropdown">
@@ -44,30 +83,42 @@
   <div class="navbar-center">
     <a class="btn btn-ghost text-xl">TESThub</a>
   </div>
+  <div class="dropdown dropdown-end">
+    {#if me}
+    <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+      <div class="w-10 rounded-full">
+        <img src={me.photoURL} alt="Rounded avatar" />
+      </div>
+    </div>
+    <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+      <li>
+        <a class="justify-between">
+          Profile
+          <span class="badge">New</span>
+        </a>
+      </li>
+      <li><a>Settings</a></li>
+      <li on:click={logOut}>Logout</li>
+    </ul>
+    {:else}
+    <div><a href="/auth">Login</a></div>
+  </div>
   <div class="navbar-end">
-    <button class="btn btn-ghost btn-circle">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-    </button>
     <button class="btn btn-ghost btn-circle">
       <div class="indicator">
         <select
-		data-choose-theme
-		class="select select-bordered select-primary mx-auto my-5 w-full max-w-3xl text-xl capitalize"
-		on:change={setTheme}
-	>
-		<option disabled selected>Choose a theme</option>
-		{#each themes as theme}
-			<option value={theme} class="capitalize">{theme}</option>
-		{/each}
-	</select>
+          data-choose-theme
+          class="select select-bordered select-primary mx-auto my-5 w-full max-w-3xl text-xl capitalize"
+          on:change={setTheme}
+        >
+          <option disabled selected>Choose a theme</option>
+          {#each themes as theme}
+          <option value={theme} class="capitalize">{theme}</option>
+          {/each}
+        </select>
       </div>
     </button>
   </div>
 </div>
 
-
-
 <slot />
-
-
-
