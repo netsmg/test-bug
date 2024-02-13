1fb927b5e9667d9f9e328c5d48baf5e36d800949
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-13 20:37:31 +0600

    Create +page.svelte

---
+++ src/routes/beta/+page.svelte
@@ -0,0 +1,58 @@
+<div class="bg-gray-200 max-w-sm shadow-lg rounded-3xl">
+  <div class="p-3.5">
+    <div class="relative">
+      <div>
+        <img src="https://images.unsplash.com/photo-1488802484013-f23aca1e2d9b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmxhbWluZ298ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="Flamingo" class="rounded-3xl w-full h-96 object-cover shadow-lg">
+      </div>
+      <div class="flex items-center justify-between px-5 py-3 text-white absolute top-0 left-0 right-0">
+        <div>
+          <div class="bg-white rounded-full w-8 h-8 flex justify-center items-center">
+            <svg class="w-5 h-5 text-yellow-500 cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path></svg>
+          </div>
+        </div>
+        <div class="bg-white rounded-full w-8 h-8 flex justify-center items-center">
+          <svg class="w-5 h-5 text-yellow-500 cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="px-7 py-5">
+    <div class="flex items-center justify-between">
+      <h2 class="text-gray-900 font-bold text-2xl">Flamingo</h2>
+      <span class="text-gray-900 font-bold text-2xl">$250</span>
+    </div>
+    <p class="flex items-center text-sm text-gray-500 mt-2">
+      <svg class="w-4 h-4 mr-1 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
+      </svg>South Africa
+    </p>
+    <ul class="text-xs text-gray-500 flex items-center justify-between my-6">
+      <li>African Travel</li>
+      <span class="w-0.5 h-3 bg-gray-400 block"></span>
+      <li>7 Day and nights</li>
+      <span class="w-0.5 h-3 bg-gray-400 block"></span>
+      <li>5 Star Hotel</li>
+      <span class="w-0.5 h-3 bg-gray-400 block"></span>
+      <li>2 Person</li>
+    </ul>
+    <p class="text-base text-gray-500">Flamingos or flamingoes are a type of wading bird in the family Phoenicopteridae, the only bird family in the order Phoenicopteriformes.</p>
+    <div class="grid grid-cols-2 gap-5 mt-7">
+      <div class="relative">
+        <input type="text" class="pt-2.5 pb-3 pl-9 rounded-full bg-transparent border border-gray-400 focus:outline-none w-40" placeholder="Ticket">
+        <svg class="w-5 h-5 text-gray-500 absolute top-3.5 left-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"></path></svg>
+        <svg class="w-5 h-5 text-gray-500 absolute top-3.5 right-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+      </div>
+      <div class="relative">
+        <input type="text" class="pt-2.5 pb-3 pl-9 rounded-full bg-transparent border border-gray-400 focus:outline-none w-40" placeholder="28 Feb 2021">
+        <svg class="w-5 h-5 text-gray-500 absolute top-3.5 left-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2"></path></svg>
+        <svg class="w-5 h-5 text-gray-500 absolute top-3.5 right-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+      </div>
+    </div>
+    <button class="bg-gray-900 text-gray-200 rounded-full py-5 px-10 mt-9 capitalize tracking-wider w-full font-semibold">get ticket</button>
+  </div>
+</div>
+</div>
+</div>
