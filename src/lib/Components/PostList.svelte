c9ccb2b5c9fe92a6266f25441f7a9f0966f5bfca
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-16 18:08:17 +0600

    Update PostList.svelte

--- src/lib/Components/PostList.svelte
+++ src/lib/Components/PostList.svelte
@@ -17,7 +17,7 @@
     <div class="card__body">
       {#each tags as tag, index}
         {#if index < 3}
-          <span class="tag tag-blue">{tag}</span>
+         <code> <span class="tag tag-blue">{tag}</span></code>
         {/if}
       {/each}
       <h4>{title}</h4>
@@ -30,7 +30,8 @@
           <h5>{author}</h5>
           <small>{createdAt}</small>
           <small>· {mins} min read ·</small>
-          <small><Icon icon="ic:baseline-remove-red-eye" />{read} <Icon icon="bi:dot" /> <Icon icon="cil:comment-bubble" /> {commentCount}</small>
+          <small>. {read} reads</small>
+<small>. {commentCount} comments</small>
         </div>
       </div>
     </div>
