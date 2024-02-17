dbf053d03fa497315a07ea38b77e81ac307e3249
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-17 13:44:14 +0600

    Update +page.svelte

--- src/routes/beta/+page.svelte
+++ src/routes/beta/+page.svelte
@@ -1,210 +1,125 @@
-<style>
-    @import url('https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700');
-@import url('https://fonts.googleapis.com/css?family=Raleway:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i');
+import "./page.css";
 
-*{
-  box-sizing: border-box;
-}
+<div class='blogM'>
+<!--[ Main content ]-->
+<main class='blogItm mainbar'>
+<div class='sldO scrlH no-items section' id='slider-widget'>
+</div>
+<div class='section' id='top-widget'>
+<div class='widget FeaturedPost' data-version='2' id='FeaturedPost1'>
+<h2 class='title dt'>Pinned Post</h2>
+<div class='itemFt' role='feed'>
 
-body, html {
-   font-family: 'Roboto Slab', serif;
-    margin: 0;
-  width: 100%;
-height: 100%;
-    padding: 0;
-}
+<article class='itm'>
+<div class='iThmb pThmb' >
+<a class='thmb' href='/'>
 
-body {
-  background-color: #D2DBDD;
-  display: flex;
-  display: -webkit-flex;
-  -webkit-justify-content: center;
-  -webkit-align-items: center;
-  justify-content: center;
-  align-items: center;
-}
-
-.cards {
-    width: 100%;
-    display: flex;
-    display: -webkit-flex;
-    justify-content: center;
-    -webkit-justify-content: center;
-    max-width: 820px;
-}
-
-.card--1 .card__img, .card--1 .card__img--hover {
-    background-image: url('https://images.pexels.com/photos/45202/brownie-dessert-cake-sweet-45202.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260');
-}
-
-.card--2 .card__img, .card--2 .card__img--hover {
-    background-image: url('https://images.pexels.com/photos/307008/pexels-photo-307008.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260');
-}
-
-.card__like {
-    width: 18px;
-}
-
-.card__clock {
-    width: 15px;
-  vertical-align: middle;
-    fill: #AD7D52;
-}
-.card__time {
-    font-size: 12px;
-    color: #AD7D52;
-    vertical-align: middle;
-    margin-left: 5px;
-}
-
-.card__clock-info {
-    float: right;
-}
-
-.card__img {
-  visibility: hidden;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    width: 100%;
-    height: 235px;
-  border-top-left-radius: 12px;
-border-top-right-radius: 12px;
-  
-}
-
-.card__info-hover {
-    position: absolute;
-    padding: 16px;
-  width: 100%;
-  opacity: 0;
-  top: 0;
-}
-
-.card__img--hover {
-  transition: 0.2s all ease-out;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    width: 100%;
-  position: absolute;
-    height: 235px;
-  border-top-left-radius: 12px;
-border-top-right-radius: 12px;
-top: 0;
-  
-}
-.card {
-  margin-right: 25px;
-  transition: all .4s cubic-bezier(0.175, 0.885, 0, 1);
-  background-color: #fff;
-    width: 33.3%;
-  position: relative;
-  border-radius: 12px;
-  overflow: hidden;
-  box-shadow: 0px 13px 10px -7px rgba(0, 0, 0,0.1);
-}
-.card:hover {
-  box-shadow: 0px 30px 18px -8px rgba(0, 0, 0,0.1);
-    transform: scale(1.10, 1.10);
-}
-
-.card__info {
-z-index: 2;
-  background-color: #fff;
-  border-bottom-left-radius: 12px;
-border-bottom-right-radius: 12px;
-   padding: 16px 24px 24px 24px;
-}
-
-.card__category {
-    font-family: 'Raleway', sans-serif;
-    text-transform: uppercase;
-    font-size: 13px;
-    letter-spacing: 2px;
-    font-weight: 500;
-  color: #868686;
-}
-
-.card__title {
-    margin-top: 5px;
-    margin-bottom: 10px;
-    font-family: 'Roboto Slab', serif;
-}
-
-.card__by {
-    font-size: 12px;
-    font-family: 'Raleway', sans-serif;
-    font-weight: 500;
-}
-
-.card__author {
-    font-weight: 600;
-    text-decoration: none;
-    color: #AD7D52;
-}
-
-.card:hover .card__img--hover {
-    height: 100%;
-    opacity: 0.3;
-}
-
-.card:hover .card__info {
-    background-color: transparent;
-    position: relative;
-}
-
-.card:hover .card__info-hover {
-    opacity: 1;
-}
-
-</style>
-<section class="cards">
-<article class="card card--1">
-  <div class="card__info-hover">
-    <svg class="card__like"  viewBox="0 0 24 24">
-    <path fill="#000000" d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z" />
+</a>
+<div class='iFxd'>
+    <span aria-label='1 mins read' bm-id='5529195070585307111' bm-img='' class='bM bmPs' data-added='Remove' role='button'>
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 50 50">
+<path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 37.039062 10.990234 A 1.0001 1.0001 0 0 0 36.265625 11.322266 L 26.183594 22.244141 A 3 3 0 0 0 25 22 A 3 3 0 0 0 22 25 A 3 3 0 0 0 25 28 A 3 3 0 0 0 25.5 27.958984 L 29.125 34.486328 A 1.0010694 1.0010694 0 1 0 30.875 33.513672 L 27.246094 26.984375 A 3 3 0 0 0 28 25 A 3 3 0 0 0 27.652344 23.599609 L 37.734375 12.677734 A 1.0001 1.0001 0 0 0 37.039062 10.990234 z"></path>
 </svg>
-      <div class="card__clock-info">
-        <svg class="card__clock"  viewBox="0 0 24 24"><path d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z" />
-        </svg><span class="card__time">15 min</span>
-      </div>
-    
-  </div>
-  <div class="card__img"></div>
-  <a href="#" class="card_link">
-     <div class="card__img--hover"></div>
-   </a>
-  <div class="card__info">
-    <span class="card__category"> Recipe</span>
-    <h3 class="card__title">Crisp Spanish tortilla Matzo brei</h3>
-    <span class="card__by">by <a href="#" class="card__author" title="author">Celeste Mills</a></span>
-  </div>
+</span>
+    <span aria-label='3 comment'bm-id='5529195070585307111' bm-img='' class='bM bmPs' data-added='Remove' role='button'>
+    <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <g transform='translate(2.000000, 4.000000)'><path d="M12,2A10,10,0,0,0,2.44,14.91a.29.29,0,0,0,0,.1,9.83,9.83,0,0,0,1.79,3.32l-2,2a1,1,0,0,0-.21,1.09A1,1,0,0,0,3,22h9a10,10,0,0,0,9.46-6.78l0-.08A9.89,9.89,0,0,0,22,12,10,10,0,0,0,12,2Zm0,18H5.41l.3-.29,8.41-8.42a1,1,0,0,1,1.4,0l3.62,3.6.23.22A8,8,0,0,1,12,20ZM4.54,14.87l1.58-1.58a1,1,0,0,1,1.41,0l.87.87L5.68,16.9A7.67,7.67,0,0,1,4.54,14.87Zm15.41-2-3-3a3,3,0,0,0-4.24,0L9.82,12.76l-.88-.87a3,3,0,0,0-4.23,0L4,12.56A5.25,5.25,0,0,1,4,12a8,8,0,0,1,16,0A8.27,8.27,0,0,1,20,12.86Z" fill="blue"></path></g></svg></span>
+
+<span aria-label='13 views' bm-id='5529195070585307111' bm-img='' class='bM bmPs' data-added='Remove' role='button'>
+<svg class='line' viewBox='0 0 24 24'><g transform='translate(2.000000, 4.000000)'><path class='svgC' d='M13.1643,8.0521 C13.1643,9.7981 11.7483,11.2141 10.0023,11.2141 C8.2563,11.2141 6.8403,9.7981 6.8403,8.0521 C6.8403,6.3051 8.2563,4.8901 10.0023,4.8901 C11.7483,4.8901 13.1643,6.3051 13.1643,8.0521 Z'></path><path d='M0.7503,8.0521 C0.7503,11.3321 4.8923,15.3541 10.0023,15.3541 C15.1113,15.3541 19.2543,11.3351 19.2543,8.0521 C19.2543,4.7691 15.1113,0.7501 10.0023,0.7501 C4.8923,0.7501 0.7503,4.7721 0.7503,8.0521 Z'></path></g></svg>
+</span>
+</div>
+<div class='iFxd bl'>
+<div class='aNm t'>
+<bdi class='nm' data-text='PWTBLOG'></bdi>
+<svg viewBox='0 0 24 24'><path d='M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z'></path></svg>
+</div>
+</div>
+<div class='blog-admin'>
+<div class='iFxd l'>
+<a aria-label='Edit this post' class='edit' data-text='Edit' href='' role='button' target='_blank'>
+<svg class='line' viewBox='0 0 24 24'><g transform='translate(3.500000, 3.500000)'><line class='svgC' x1='9.8352' x2='16.2122' y1='16.0078' y2='16.0078'></line><path d='M12.5578,1.3589 L12.5578,1.3589 C11.2138,0.3509 9.3078,0.6229 8.2998,1.9659 C8.2998,1.9659 3.2868,8.6439 1.5478,10.9609 C-0.1912,13.2789 1.4538,16.1509 1.4538,16.1509 C1.4538,16.1509 4.6978,16.8969 6.4118,14.6119 C8.1268,12.3279 13.1638,5.6169 13.1638,5.6169 C14.1718,4.2739 13.9008,2.3669 12.5578,1.3589 Z'></path><line x1='7.0041' x2='11.8681' y1='3.7114' y2='7.3624'></line></g></svg>
+</a>
+</div>
+</div>
+</div>
+<div class='iCtnt'>
+<div class='pHdr pSml'>
+<div class='pLbls' data-text='in'>
+<a aria-label='css' data-text='html' href='' rel='tag'>
+</a>
+<a aria-label='glowing' data-text='glowing' href=''>
+</a>
+</div>
+</div>
+<h3 class='pTtl aTtl'><a href=''>Creating A Glowing Loading Spinner Using HTML And CSS</a></h3>
+<div class='pSnpt'>
+Introduction In the ever-evolving landscape of web development, creating visually appealing and engaging user interfaces has become paramount. Loadi&#8230;
+</div>
+<div class='pInf pSml'>
+<time class='aTtmp pTtmp pbl' data-text='Feb 10, 2024' data-type='Published' datetime='2024-02-10T16:21:00+05:30' title='Published: February 10, 2024'></time>
+<a aria-label='Read more' class='pJmp' data-text='Keep reading' href=''></a>
+</div>
+</div>
 </article>
-  
-  
-<article class="card card--2">
-  <div class="card__info-hover">
-    <svg class="card__like"  viewBox="0 0 24 24">
-    <path fill="#000000" d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z" />
+
+</br><article class='itm'>
+<div class='iThmb pThmb'>
+<a class='thmb' href='/'>
+
+</a>
+<div class='iFxd'>
+    <span aria-label='1 mins read' bm-id='5529195070585307111' bm-img='' class='bM bmPs' data-added='Remove' role='button'>
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 50 50">
+<path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 37.039062 10.990234 A 1.0001 1.0001 0 0 0 36.265625 11.322266 L 26.183594 22.244141 A 3 3 0 0 0 25 22 A 3 3 0 0 0 22 25 A 3 3 0 0 0 25 28 A 3 3 0 0 0 25.5 27.958984 L 29.125 34.486328 A 1.0010694 1.0010694 0 1 0 30.875 33.513672 L 27.246094 26.984375 A 3 3 0 0 0 28 25 A 3 3 0 0 0 27.652344 23.599609 L 37.734375 12.677734 A 1.0001 1.0001 0 0 0 37.039062 10.990234 z"></path>
 </svg>
-      <div class="card__clock-info">
-        <svg class="card__clock"  viewBox="0 0 24 24"><path d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z" />
-        </svg><span class="card__time">5 min</span>
-      </div>
-    
-  </div>
-  <div class="card__img"></div>
-  <a href="#" class="card_link">
-     <div class="card__img--hover"></div>
-   </a>
-  <div class="card__info">
-    <span class="card__category"> Travel</span>
-    <h3 class="card__title">Discover the sea</h3>
-    <span class="card__by">by <a href="#" class="card__author" title="author">John Doe</a></span>
-  </div>
-</article>  
-  
-  
-  
-  </section>
+</span>
+    <span aria-label='3 comment'bm-id='5529195070585307111' bm-img='' class='bM bmPs' data-added='Remove' role='button'>
+    <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <g transform='translate(2.000000, 4.000000)'><path d="M12,2A10,10,0,0,0,2.44,14.91a.29.29,0,0,0,0,.1,9.83,9.83,0,0,0,1.79,3.32l-2,2a1,1,0,0,0-.21,1.09A1,1,0,0,0,3,22h9a10,10,0,0,0,9.46-6.78l0-.08A9.89,9.89,0,0,0,22,12,10,10,0,0,0,12,2Zm0,18H5.41l.3-.29,8.41-8.42a1,1,0,0,1,1.4,0l3.62,3.6.23.22A8,8,0,0,1,12,20ZM4.54,14.87l1.58-1.58a1,1,0,0,1,1.41,0l.87.87L5.68,16.9A7.67,7.67,0,0,1,4.54,14.87Zm15.41-2-3-3a3,3,0,0,0-4.24,0L9.82,12.76l-.88-.87a3,3,0,0,0-4.23,0L4,12.56A5.25,5.25,0,0,1,4,12a8,8,0,0,1,16,0A8.27,8.27,0,0,1,20,12.86Z" fill="blue"></path></g></svg></span>
+
+<span aria-label='13 views' bm-id='5529195070585307111' bm-img='' class='bM bmPs' data-added='Remove' role='button'>
+<svg class='line' viewBox='0 0 24 24'><g transform='translate(2.000000, 4.000000)'><path class='svgC' d='M13.1643,8.0521 C13.1643,9.7981 11.7483,11.2141 10.0023,11.2141 C8.2563,11.2141 6.8403,9.7981 6.8403,8.0521 C6.8403,6.3051 8.2563,4.8901 10.0023,4.8901 C11.7483,4.8901 13.1643,6.3051 13.1643,8.0521 Z'></path><path d='M0.7503,8.0521 C0.7503,11.3321 4.8923,15.3541 10.0023,15.3541 C15.1113,15.3541 19.2543,11.3351 19.2543,8.0521 C19.2543,4.7691 15.1113,0.7501 10.0023,0.7501 C4.8923,0.7501 0.7503,4.7721 0.7503,8.0521 Z'></path></g></svg>
+</span>
+</div>
+<div class='iFxd bl'>
+<div class='aNm t'>
+<bdi class='nm' data-text='PWTBLOG'></bdi>
+<svg viewBox='0 0 24 24'><path d='M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z'></path></svg>
+</div>
+</div>
+<div class='blog-admin'>
+<div class='iFxd l'>
+<a aria-label='Edit this post' class='edit' data-text='Edit' href='' role='button' target='_blank'>
+<svg class='line' viewBox='0 0 24 24'><g transform='translate(3.500000, 3.500000)'><line class='svgC' x1='9.8352' x2='16.2122' y1='16.0078' y2='16.0078'></line><path d='M12.5578,1.3589 L12.5578,1.3589 C11.2138,0.3509 9.3078,0.6229 8.2998,1.9659 C8.2998,1.9659 3.2868,8.6439 1.5478,10.9609 C-0.1912,13.2789 1.4538,16.1509 1.4538,16.1509 C1.4538,16.1509 4.6978,16.8969 6.4118,14.6119 C8.1268,12.3279 13.1638,5.6169 13.1638,5.6169 C14.1718,4.2739 13.9008,2.3669 12.5578,1.3589 Z'></path><line x1='7.0041' x2='11.8681' y1='3.7114' y2='7.3624'></line></g></svg>
+</a>
+</div>
+</div>
+</div>
+<div class='iCtnt'>
+<div class='pHdr pSml'>
+<div class='pLbls' data-text='in'>
+<a aria-label='css' data-text='html' href='' rel='tag'>
+</a>
+<a aria-label='glowing' data-text='glowing' href=''>
+</a>
+</div>
+</div>
+<h3 class='pTtl aTtl'><a href=''>Creating A Glowing Loading Spinner Using HTML And CSS</a></h3>
+<div class='pSnpt'>
+Introduction In the ever-evolving landscape of web development, creating visually appealing and engaging user interfaces has become paramount. Loadi&#8230;
+</div>
+<div class='pInf pSml'>
+<time class='aTtmp pTtmp pbl' data-text='Feb 10, 2024' data-type='Published' datetime='2024-02-10T16:21:00+05:30' title='Published: February 10, 2024'></time>
+<a aria-label='Read more' class='pJmp' data-text='Keep reading' href=''></a>
+</div>
+</div>
+</article>
+
+</div>
+</div>
+</div>
+</main>
+</div>
+
+
